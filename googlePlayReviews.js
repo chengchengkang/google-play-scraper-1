48084b12eda30b5b4da40585e1239921e5cf734b
Hesam Emadi <sema523@aucklanduni.ac.nz>
2018-04-30 23:52:57 +1200

    A temp commit. Improved logging. WIP.

--- googlePlayReviews.js
+++ googlePlayReviews.js
@@ -1,8 +1,42 @@
 var gplay = require("google-play-scraper");
-function addPromise(){
+
+var APP_ID = 'com.discord';
+var addedPromises = 0;
+var resolvedPromises = 0;
+
+
+console.log("** App ID: " + APP_ID);
+console.error("** App ID: " + APP_ID);
+
+for (var i = 0; i <= 111; i++) {
+	var a = Promise.resolve(gplay.reviews({
+	  appId: APP_ID, //change this to change the app
+	  throttle: 1, //this limits the number of requests to the Google Play Store (e.g. 10 = 10 requests per second). 10 may be too high if you're planning to do >111 pages.
+	  page: i,
+	  sort: gplay.sort.NEWEST //can be HELPFULNESS, NEWEST, OR RATING
+	}));
+	addedPromises++;
+	a.then(function(value) {
+		resolvedPromises++;
+		console.log("** Promise status:: One Promise resolved, resolved promises: " + resolvedPromises + 
+		 	", promise left: " + (addedPromises - resolvedPromises) );
+		console.error("** Promise status:: One Promise resolved, resolved promises: " + resolvedPromises + 
+			", promise left: " + (addedPromises - resolvedPromises) );
+
+		for (var j = 0; j < Object.keys(value).length; j++) {
+			console.log(value[j].id + ", "+ value[j].userName + "," + value[j].score + ", " + value[j].date  +", "+value[j].text); 
+			//console.log(value[j].text); //value[j].<INSERT TAG> to change what you get from the JSON
+		}
+	});
+	console.log("** Promise status:: total promises: " + addedPromises + ", resolved promises: " + resolvedPromises);
+}
+
+// console.log(Object.getOwnPropertyNames(Promise));
+
+/*function addPromise(){
 
 	var a = Promise.resolve(gplay.reviews({
-	  appId: 'com.facebook.katana', //change this to change the app
+	  appId: 'com.sevenminworkout', //change this to change the app
 	  throttle: 1, //this limits the number of requests to the Google Play Store (e.g. 10 = 10 requests per second). 10 may be too high if you're planning to do >111 pages.
 	  page: loopCounter,
 	  sort: gplay.sort.NEWEST //can be HELPFULNESS, NEWEST, OR RATING
@@ -21,30 +55,24 @@ function myLoop () {
     setTimeout(function () {
         addPromise(); 
         loopCounter++;
-        if (loopCounter < 500) {
+        if (loopCounter < 111) {
             myLoop();
         }
-     }, 450);
+     }, 300);
 }
 
 myLoop();
-
+*/
 /*
-for (var i = 0; i <= 100; i++) {
-	// Promise.
-	setTimeout(function(){}, 300);
-	var a = Promise.resolve(gplay.reviews({
-	  appId: 'com.facebook.katana', //change this to change the app
-	  throttle: 1, //this limits the number of requests to the Google Play Store (e.g. 10 = 10 requests per second). 10 may be too high if you're planning to do >111 pages.
-	  page: i,
-	  sort: gplay.sort.HELPFULNESS //can be HELPFULNESS, NEWEST, OR RATING
-	}));
-	
-	a.then(function(value) {
-		for (var j = 0; j < Object.keys(value).length; j++) {
-			console.log(value[j].text); //value[j].<INSERT TAG> to change what you get from the JSON
-		}
-	});
-}*/
+
+Apps:
+com.androbaby.game2048
+com.sevenminworkout
+
+com.gaiam.yogastudio
+
+*/
+
+
 
 
